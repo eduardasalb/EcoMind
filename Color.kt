28cd4f9a6477c2e8831422a92a969ad152b8acf7
Maria Eduarda S. de Albuquerque <143506394+eduardasalb@users.noreply.github.com>
2025-12-10 13:37:26 -0300

    ui.theme

---
+++ Color.kt
@@ -0,0 +1,10 @@
+package com.example.ecomind.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val EcoGreen = Color(0xFF2E7D32)
+val EcoLightGreen = Color(0xFF81C784)
+val EcoDark = Color(0xFF1B5E20)
+val EcoSurface = Color(0xFFF1F8F5)
+val EcoOnSurface = Color(0xFF0B3A1B)
+val EcoAccent = Color(0xFF4CAF50)
